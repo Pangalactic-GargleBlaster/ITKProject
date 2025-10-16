28a21242dfb0c4d0d050603a5e21428e671b6722
Pangalactic-GargleBlaster <paolobuts@gmail.com>
2025-10-16 11:19:24 -0700

    reading pixels

--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 import SimpleITK as sitk
 from matplotlib import pyplot
 image_viewer = sitk.ImageViewer()
-image_viewer.SetApplication("C:/Users/admin/Desktop/821/ImageJ/ImageJ.exe")
+image_viewer.SetApplication('C:\\Users\\admin\\AppData\\Local\\Fiji\\fiji-windows-x64.exe')
 dicom_directory = "C:/Users/admin/Desktop/821/CovidScans/manifest-1608266677008/MIDRC-RICORD-1A/MIDRC-RICORD-1A-419639-000082/08-02-2002-NA-CT CHEST WITHOUT CONTRAST-04614/3.000000-0.625mm bone alg-26970/"
 series_IDs = sitk.ImageSeriesReader.GetGDCMSeriesIDs(dicom_directory)
 print(series_IDs)
@@ -10,3 +10,4 @@ reader.SetFileNames(sitk.ImageSeriesReader.GetGDCMSeriesFileNames(dicom_director
 image = reader.Execute()
 print(f"Image size: {image.GetSize()}")
 print(f"Image spacing: {image.GetSpacing()}")
+print(f"First pixel: {image.GetPixel([0,0,0])}")
